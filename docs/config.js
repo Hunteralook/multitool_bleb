cf12fbca84204a9da7971278805c9ebede8cdd16
Hunteralook <nutherbadgames12@gmail.com>
2026-07-29 17:47:51 +0700

    11
    11
    1

--- docs/config.js
+++ docs/config.js
@@ -3,7 +3,11 @@ window.UNISONO_CONFIG = Object.freeze({
   whitelistFile: "WhiteList.lua",
   logsFile: "usage_logs.json",
   commandsFile: "client_commands.json",
+<<<<<<< HEAD
   recommendedClientVersion: "v1.7.5",
+=======
+  recommendedClientVersion: "v1.7.2",
+>>>>>>> parent of 5ae7601 (123)
   maxLogs: 500,
   maxCommands: 100,
   authorizedUsers: Object.freeze({
